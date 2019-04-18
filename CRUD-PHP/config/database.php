fe12fe8722f822fa888dc3649fd591dd1105c23c
muhwezi deo <aggrey256@gmail.com>
2019-04-18 20:24:37 +0300

    add form

--- CRUD-PHP/config/database.php
+++ CRUD-PHP/config/database.php
@@ -16,6 +16,7 @@ class Database
             echo "Connection Error".$exception->getMessage();
 
         }
+
         return $this->conn;
 
     }
--- CRUD-PHP/create_product.php
+++ CRUD-PHP/create_product.php
@@ -25,8 +25,45 @@ include_once "layout_header.php";
 echo "<div class='right-button-margin'>";
 echo "<a href='index.php' class='btn btn-default pull-right'>Read Products</a>";
 echo "</div>";
+?>
+    <!-- PHP post code will be here -->
 
+    <!-- HTML form for creating a product -->
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 
-include_once "layout_footer.php";
+        <table class='table table-hover table-responsive table-bordered'>
+
+            <tr>
+                <td>Name</td>
+                <td><input type='text' name='name' class='form-control' /></td>
+            </tr>
+
+            <tr>
+                <td>Price</td>
+                <td><input type='text' name='price' class='form-control' /></td>
+            </tr>
+
+            <tr>
+                <td>Description</td>
+                <td><textarea name='description' class='form-control'></textarea></td>
+            </tr>
+
+            <tr>
+                <td>Category</td>
+                <td>
+                    <!-- categories from database will be here -->
+                </td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td>
+                    <button type="submit" class="btn btn-primary">Create</button>
+                </td>
+            </tr>
+
+        </table>
+    </form>
+<?php include_once "layout_footer.php";
 
 
