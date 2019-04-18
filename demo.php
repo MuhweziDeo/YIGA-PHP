534dd92cb09231da459bce646d95f7e129b12e36
muhwezi deo <aggrey256@gmail.com>
2019-04-18 19:35:55 +0300

    init repo

---
+++ demo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dee
+ * Date: 2019-04-17
+ * Time: 20:24
+ */
+echo 'hello world';
+$host = "localhost";
+$db_name = "Db";
+$username = "root";
+$password = "";
+
